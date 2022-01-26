71138a5f5a7c58b1c039c7e8f8e93041ef650980
Simon <simondurnitsin@gmail.com>
2022-01-26 13:49:21 +0500

    updated

--- public/redis.adapter.ts
+++ public/redis.adapter.ts
@@ -8,7 +8,7 @@ import * as redisIoAdapter from 'socket.io-redis';
 export class RedisIoAdapter extends IoAdapter {
 	createIOServer(port: number, options?: any): any {
 		const server = super.createIOServer(port, options);
-		const redisAdapter = redisIoAdapter({ host: 'localhost', port: 6379 });
+		const redisAdapter = redisIoAdapter;
 
 		server.adapter(redisAdapter);
 		return server;
